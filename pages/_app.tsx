7e7f8222c2580c54c6cf5e08f33688b8328d2bc4
Eiermann Patrick <eiermann.patrick.pro@outlook.com>
2022-03-08 20:33:07 +0100

    adding calendar

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import "@fullcalendar/common/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
